09abacfa710671fec3e9f6c2143b721ca9fb2ac4
William Dunston <wedunsto@aggies.ncat.edu>
2023-12-20 15:59:04 -0500

    Added functions

--- introduction_code/hello_world.rb
+++ introduction_code/hello_world.rb
@@ -1,3 +1,18 @@
 print "Hello world" # Display with no new line
 puts "Hello world" # Display with new line
 p "Hello world" # Return the given argument (Will include quotation marks)
+
+# Define method with no parameters
+def say_hello_world
+    puts "Hello world"
+end
+
+# Define method with parameters
+def say_hello(greetings)
+    puts greetings
+end
+
+# Call a method
+say_hello_world
+
+say_hello "Hi William"
