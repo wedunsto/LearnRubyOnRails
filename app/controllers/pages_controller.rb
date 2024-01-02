2247ea798b238eb163c158f19859870ffba68d3e
William Dunston <wedunsto@aggies.ncat.edu>
2024-01-02 14:28:34 -0500

    Practiced Creating additional routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
     def hello # Points to the hello view
     end
+
+    def about # Points to the about view
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "pages#hello" # Pages controller; hello action
+  get 'about', to: 'pages#about' # Route to pages controller; about action
 end
