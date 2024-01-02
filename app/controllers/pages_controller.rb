55cf108da71d2de7f7d8e4efeba5a6a542a70bbc
William <wedunsto@aggies.ncat.edu>
2024-01-02 11:59:28 -0500

    Practiced establishing Rails application

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+    def hello # Points to the hello view
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root "pages#hello" # Pages controller; hello action
+end
